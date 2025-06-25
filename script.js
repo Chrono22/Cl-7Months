ef06e4fbcdb4c45031bdd4877f00e8b99b320392
Verril Savero Nathannael <verrilsaveronathannael@gmail.com>
2025-06-25 20:11:45 +0700

    newest

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
+let messageShown = false;
+
 function showMessage(){
+    if(messageShown) return;
+    messageShown = true;
+
     const msg = "Kita bagaikan bla bla bla aku tidak tahu bagaiaman";
     const target = document.getElementById("secret-message");
     target.textContent = "";
@@ -14,3 +19,57 @@ function showMessage(){
         }
     }, 70);
 }
+
+const galleryImages = [
+    "Photos/Foto1.jpg",
+    "Photos/Foto2.jpg",
+    "Photos/Foto3.jpg",
+    "Photos/Foto4.jpg"
+];
+
+let currentImageIdx = 0;
+
+function showSlide(index) {
+    const img = document.querySelector(".gallery-image");
+
+    img.style.opacity = 0;
+
+    setTimeout(() => {
+        img.src = galleryImages[index];
+        img.onload = () => {
+            img.style.opacity = 1;
+        };
+    }, 200);
+}
+
+function prevSlide() {
+    currentImageIdx = (currentImageIdx - 1 + galleryImages.length) % galleryImages.length;
+    showSlide(currentImageIdx);
+}
+
+function nextSlide() {
+    currentImageIdx = (currentImageIdx + 1) % galleryImages.length;
+    showSlide(currentImageIdx);
+}
+
+const emojiList = ["💖", "🌸", "✨", "💘", "💞"];
+
+function createHeart() {
+    console.log("Hati")
+    const heart = document.createElement("div");
+    heart.classList.add("heart");
+    
+    const randomEmoji = emojiList[Math.floor(Math.random() * emojiList.length)]
+    heart.innerHTML = randomEmoji;
+
+    heart.style.left = Math.random() * 100 + "vw";
+    heart.style.fontSize = Math.random() * 10 + 15 + "px";
+
+    document.querySelector(".hearts-container").appendChild(heart);
+
+    setTimeout(() => {
+        heart.remove();
+    }, 5000);
+}
+
+setInterval(createHeart, 300);
